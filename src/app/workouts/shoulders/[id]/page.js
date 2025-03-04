1c66e0ace6caad9c43436fb216218da7ed8ad30b
Mauro D'Abreu Da Grace Trovoada (trovoadm) <130519163+MauroT11@users.noreply.github.com>
2025-03-04 20:14:38 +0000

    hotfix, React hook dependency issue

--- src/app/workouts/shoulders/[id]/page.js
+++ src/app/workouts/shoulders/[id]/page.js
@@ -56,7 +56,7 @@ export default function Page({params}) {
         fetchWorkout();
         fetchInstructions();
         fetchExerEquipment();
-      }, []);
+      }, [params.id]);
 
     return (
         <div>
